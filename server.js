040b4492104332103a3d52c754cc3bfc96f186b7
ongsterr <ong.chris11@gmail.com>
2018-06-22 09:11:14 +1000

    Update router path

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const router = require('./router');
+const router = require('./backend/router');
 const path = require('path')
 require('dotenv').config();
 
